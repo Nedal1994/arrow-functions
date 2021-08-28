382d0d0b764873e906a4801b2183885e6e79e383
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-08-28 20:25:30 +0300

    ok

---
+++ app.js
@@ -0,0 +1,14 @@
+// normal function
+
+hello = function()
+{
+    return "My name is Nedal Al Saleh"
+}
+
+// arrow function
+intro = () =>
+{
+    return "I'm a student at LTUC studying software development"
+}
+// console.log(hello);
+// console.log(intro);
